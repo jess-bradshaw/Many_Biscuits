fbb6d4732206932626bd7fdb329a9ab06d04a638
Jess Bradshaw <134118247+jess-bradshaw@users.noreply.github.com>
2023-12-04 20:39:17 -0500

    Added in main menu option/scene. Updated some prefabs. added animation for wisp again.

---
+++ Main/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+   public void LoadScene(int level)
+   {
+    Application.LoadLevel(level); 
+    
+   }
+
+    public void QuitGame()
+   {
+    Application.Quit(); 
+    
+   }
+}
