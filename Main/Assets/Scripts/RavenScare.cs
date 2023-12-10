fdf5eba4d5ac3a5ef0cc7991be190a1ccc36c591
Jess Bradshaw <134118247+jess-bradshaw@users.noreply.github.com>
2023-12-10 13:02:13 -0500

    Pseudo code ravens.

---
+++ Main/Assets/Scripts/RavenScare.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RavenScare : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if collider is triggered {on trigger enter}
+        // then scare the raven + mark as scared away 
+
+        //if raven present and Wisp in trigger hold then scared emote. + decrease emotions by one level only. 
+    }
+}
